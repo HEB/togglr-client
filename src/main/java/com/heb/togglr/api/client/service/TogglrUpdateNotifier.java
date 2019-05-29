4d953f82ea06307b7fe9d1a387477c4601bcd7f2
g592345 <gorena.david@heb.com>
2019-05-29 09:12:42 -0500

    Updating to add in the a cache timer.
    Updating more documentation.

--- src/main/java/com/heb/togglr/api/client/service/TogglrUpdateNotifier.java
+++ src/main/java/com/heb/togglr/api/client/service/TogglrUpdateNotifier.java
@@ -1,48 +1,63 @@
 package com.heb.togglr.api.client.service;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TogglrUpdateNotifier {
 
+    @Value("${heb.togglr.cache-time:#{0}}")
+    private long cacheTime;
+
     private long updateCount = 0;
     private Map<String, Long> lastUpdateReceivedd;
+    private Map<String, Long> lastUpdateTime;
 
     public TogglrUpdateNotifier(){
         this.lastUpdateReceivedd = new HashMap<>();
+        this.lastUpdateTime = new HashMap<>();
     }
 
     public void registerNewUpdate(){
         this.updateCount++;
+        this.clearCache();
     }
 
     /**
      * Returns True if the user needs to have their togglr items refreshed.
-     * @param userId
+     * @param cacheId
      * @return
      */
-    public boolean doesClientNeedUpdate(String userId){
-        Long lastUpdate = lastUpdateReceivedd.get(userId);
+    public boolean doesClientNeedUpdate(String cacheId){
+        Long lastUpdate = this.lastUpdateReceivedd.get(cacheId);
+        Long lastUpdateTime = this.lastUpdateTime.get(cacheId);
 
-        if(lastUpdate == null){
+
+        if(lastUpdate == null || lastUpdate < updateCount || lastUpdateTime > cacheTime){
             return true;
         }
 
-        if(lastUpdate != updateCount){
-            return true;
-        }else{
-            return false;
-        }
+        return false;
     }
 
     /**
      * Stores the current update version for the user.
-     * @param userId
+     * @param cacheId
      */
-    public void updateUserVersion(String userId){
-        this.lastUpdateReceivedd.put(userId, this.updateCount);
+    public void updateUserVersion(String cacheId){
+        this.lastUpdateReceivedd.put(cacheId, this.updateCount);
+        this.lastUpdateTime.put(cacheId, Instant.now().toEpochMilli());
+    }
+
+    /**
+     * Reset the maps used for caching.
+     */
+    public void clearCache(){
+        this.lastUpdateReceivedd.clear();
+        this.lastUpdateTime.clear();
     }
 }
